bc693a41e7d6b51178b5402205198f1dae2893ae
TheMan <ntnhacker1@gmail.com>
2025-05-16 20:24:33 +0700

    fix hiển thị giá

--- canteen/settings.py
+++ canteen/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.humanize',
     # Custom apps
     "users.apps.UsersConfig",
     "products.apps.ProductsConfig",
