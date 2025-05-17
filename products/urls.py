e4d3106781457b9d2f6a476b16331f16fbcc335e
TheMan <ntnhacker1@gmail.com>
2025-05-17 14:46:56 +0700

    input search

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
+    path('', views.home, name='home'),
     path('category/<slug:slug>/', views.CategoryProductsView.as_view(), name='category'),
     path('product/<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
 ]
