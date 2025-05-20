52e92f52f74f9dc8c774b398765d3173a5f225e6
Le Nam <lenam20092003@gmail.com>
2025-05-21 00:35:54 +0700

    host

--- canteen/settings.py
+++ canteen/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = "django-insecure-xxiy%*3=b842ueu$i)=(jxwc=4r)%b+%ygm%jhk2q+t##&8q#d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["<public-ip>", "<EC2-public-DNS>", "localhost", "127.0.0.1"]
 
 
 # Application definition
