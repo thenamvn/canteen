6d2d5111108de00e49d35d908dabf390afe0193a
Le Nam <lenam20092003@gmail.com>
2025-05-21 00:38:32 +0700

    host

--- canteen/settings.py
+++ canteen/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = "django-insecure-xxiy%*3=b842ueu$i)=(jxwc=4r)%b+%ygm%jhk2q+t##&8q#d
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "http://54.253.40.34:8000/",
+    "54.253.40.34",
     "<EC2-public-DNS>",
     "localhost",
     "127.0.0.1",
