67b3a417fcf5a382e74b509ec68ab56bca022d6c
Nhì Bùi <buiducnhi02@gmail.com>
2025-05-21 21:07:43 +0700

    canh sat chinh ta day !!

--- chat/urls.py
+++ chat/urls.py
@@ -7,6 +7,5 @@ app_name = 'chat'
 urlpatterns = [
     # URL này sẽ dùng để seller vào một phòng chat cụ thể
     path('seller/room/<int:room_id>/', views.seller_chat_room_view, name='seller_chat_room'),
-    # Bạn có thể thêm một trang liệt kê tất cả các phòng chat của seller ở đây nếu muốn
     path('seller/list/', views.seller_chat_list_view, name='seller_chat_list'),
 ]
